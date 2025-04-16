ab1bfc1be05546b035f928920ee24ca8b6713705
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-04-16 18:11:47 +0600

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>This is a simple about page for the Vue.js app.</p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="home">
     <h1>HomePage</h1>
-    <p>Welcome to your Vue.js app!</p>
+    <p>
+      Routing in Vue is used to navigate the Vue application, and it happens on
+      the client side (in the browser) without full page reload, which results
+      in a faster user experience. Routing is a way to navigate, similar to how
+      we have used dynamic components earlier. With routing we can use the URL
+      address to direct someone to a specific place in our Vue application.
+    </p>
     <p>
       For a guide and recipes on how to configure / customize this project,
       check out the
